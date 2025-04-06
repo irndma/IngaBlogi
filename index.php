fe1f93688c4cbc0bbaf92de556f01ad007b83d66
irndma <inga.randma@gmail.com>
2025-04-06 21:41:14 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$page = isset($_GET['page']) ? $_GET['page'] : 'index_'; //Kui on olemas page, siis võta see, muidu võta index
+$allowed_pages = ['index_', 'blog', 'contact', 'post1', 'post2', 'post3', 'post4', 'post5']; // .html failid
+if(!in_array($page, $allowed_pages)) { 
+    $page = 'index_'; // Kui on mingi kolmas leht peale indexi ja posti, siis alati too avalehele
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Avaleht</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <?php include 'menu.html'; ?>
+    </div>
+
+    <div class="container">
+        <?php include("$page.html"); ?>
+
+    </div>
+   
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
